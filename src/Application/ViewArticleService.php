ffaa1cfd7eeaf43bc12f297c00edddaf9eea9840
dan <danvelev9@gmail.com>
2022-03-27 00:14:04 +0100

    add View article service

---
+++ src/Application/ViewArticleService.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Application;
+
+use Src\Application\Repositories\ArticleRepository;
+use Src\Domain\Article;
+
+class ViewArticleService
+{
+    public function __construct(private ArticleRepository $repository)
+    { }
+
+    public function findArticleById(int $articleId): ?Article
+    {
+        return $this->repository->findById($articleId);
+    }
+
+}
