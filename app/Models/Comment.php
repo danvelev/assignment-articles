bcfeefeb456048eed5c2c352cada45ed62af7448
dan <danvelev9@gmail.com>
2022-03-27 00:15:35 +0100

    add Article and Comment ORM models

---
+++ app/Models/Comment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'message',
+        'visitor',
+        'date_created',
+        'date_published'
+    ];
+
+    public function article(): BelongsTo
+    {
+        return $this->belongsTo(Article::class);
+    }
+
+    public function visitor(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
