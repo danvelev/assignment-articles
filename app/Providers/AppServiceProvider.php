eea0d8896152364723f109dcd03dd1ace8c078e9
dan <danvelev9@gmail.com>
2022-03-27 18:29:55 +0200

    bind new user and comment intention repos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,11 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Src\Application\Repositories\ArticleRepository;
+use Src\Application\Repositories\CommentIntentionRepository;
+use Src\Application\Repositories\UserRepository;
 use Src\Infrastructure\Repositories\EloquentArticleRepository;
+use Src\Infrastructure\Repositories\EloquentCommentIntentionRepository;
+use Src\Infrastructure\Repositories\EloquentUserRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,6 +22,16 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(
             ArticleRepository::class,
             EloquentArticleRepository::class
+        );
+
+        $this->app->bind(
+            UserRepository::class,
+            EloquentUserRepository::class
+        );
+
+        $this->app->bind(
+            CommentIntentionRepository::class,
+            EloquentCommentIntentionRepository::class
         );
     }
 
