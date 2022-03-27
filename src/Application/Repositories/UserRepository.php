2c79c2265f45a1f1dcecab18181f985816b6a991
dan <danvelev9@gmail.com>
2022-03-27 18:17:38 +0200

    add User Repo and User service to find user by ID

---
+++ src/Application/Repositories/UserRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Src\Application\Repositories;
+
+use Src\Domain\User;
+
+interface UserRepository
+{
+    public function findById(int $userId): ?User;
+}
---
+++ src/Application/ViewUserService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Src\Application;
+
+use Src\Application\Repositories\UserRepository;
+use Src\Domain\User;
+
+class ViewUserService
+{
+    public function __construct(private UserRepository $repository)
+    { }
+
+    public function findUserById(int $userId): ?User
+    {
+        return $this->repository->findById($userId);
+    }
+
+}
