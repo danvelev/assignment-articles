d1a4fc6fdc7a1ec81da753dadfd20d0967e52001
dan <danvelev9@gmail.com>
2022-03-27 18:28:56 +0200

    Add eloq. User Repo

---
+++ src/Infrastructure/Repositories/EloquentUserRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Src\Infrastructure\Repositories;
+
+use App\Models\User as EloquentUserModel;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Src\Application\Exceptions\UserNotFoundException;
+use Src\Application\Repositories\UserRepository;
+use Src\Domain\User;
+use Src\Domain\ValueObjects\UserId;
+
+class EloquentUserRepository implements UserRepository
+{
+
+
+    public function __construct(
+        private EloquentUserModel $eloquentUserModel
+    ) { }
+
+    public function findById(int $userId): ?User
+    {
+        try {
+            $user = $this->eloquentUserModel::query()->findOrFail($userId);
+
+            return new User(
+                new UserId($user->id),
+                $user->name,
+                $user->email
+            );
+        } catch (ModelNotFoundException) {
+            throw new UserNotFoundException(
+                sprintf("No user found with %d ", $userId), 404);
+        }
+    }
+}
