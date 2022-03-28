ce0cef93a8fee382ff161c36c94edf6058133bd7
dan <danvelev9@gmail.com>
2022-03-28 21:16:56 +0200

    Add article factory for testing

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->name(),
+            'content' => $this->faker->text,
+            'user_id' => User::factory(),
+            'published_at' => $this->faker->dateTime()
+        ];
+    }
+}
