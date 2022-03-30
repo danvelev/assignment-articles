46f20669d5249bbc278ff14b78676017d70c9972
dan <danvelev9@gmail.com>
2022-03-30 20:23:20 +0200

    add unit test on article make

--- tests/Unit/ArticleTest.php
+++ tests/Unit/ArticleTest.php
@@ -2,7 +2,45 @@
 
 namespace Tests\Unit;
 
-class ArticleTest
-{
+use PHPUnit\Framework\Assert;
+use Src\Domain\Article;
+use Src\Domain\User;
+use Tests\TestCase;
 
+class ArticleTest extends TestCase
+{
+    public function testArticleMake()
+    {
+        $dateCreated = \DateTime::createFromFormat('Y-m-d H:i:s', '2022-01-01 00:00:00');
+
+        $user = User::make(1, 'name', 'email@mail.com');
+        $article = Article::make(
+            1,
+            'title',
+            'content',
+            $user,
+            $dateCreated
+        );
+
+        Assert::assertNotEmpty($article);
+        Assert::assertEquals(1, $article->id()->value());
+        Assert::assertEquals('content', $article->content()->value());
+        Assert::assertEquals($dateCreated, $article->dateCreated());
+    }
+
+    public function testArticleMakeWithoutDate()
+    {
+        $user = User::make(1, 'name', 'email@mail.com');
+        $article = Article::make(
+            1,
+            'title',
+            'content',
+            $user
+        );
+
+        Assert::assertNotEmpty($article);
+        Assert::assertEquals(1, $article->id()->value());
+        Assert::assertEquals('content', $article->content()->value());
+        Assert::assertNotEmpty($article->dateCreated());
+    }
 }
